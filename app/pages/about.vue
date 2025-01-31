1b21d0e8f67defa3b54c1888c0c70fa9015e4b22
mohammadakradi <mohammad.akradi96@gmail.com>
2025-01-31 20:59:11 +0330

    update

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -9,7 +9,7 @@
       <h1 class="text-6xl font-extrabold mb-6 text-balance">About Nuxtcipes:</h1>
       <p class="text-xl font-extrabold mb-6 text-balance max-w-screen-lg">
         Nuxtcipes is a recipe application based on the dummyJSON API, created by John Komarnicki for
-        the <span class="font-bold">Build Modern Apps with Nuxt</span> course.
+        the <span class="font-bold">Build Modern Apps with Nuxt</span> course. I created this app to learn Nuxt using yotube.
       </p>
     </section>
   </main>
